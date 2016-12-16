124178c58e129ec65fc03e2f18495cc8eba3845d
Lisuzu75 <tiagofilipegouveia@hotmail.com>
2016-12-16 13:45:05 +0000

    AYY LMAO

--- MyWeatherApp/app/src/main/java/com/example/pedrofialho/myweatherapp/presentation/SyncActivity.kt
+++ MyWeatherApp/app/src/main/java/com/example/pedrofialho/myweatherapp/presentation/SyncActivity.kt
@@ -96,8 +96,6 @@ class SyncActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
                textToPresent = "You choose WIFI option"
             } else if(selectedId == dados.id) {
                 textToPresent = "You choose Mobile Data option"
-
-
             } else {
                 textToPresent = "You choose Both options"
             }
