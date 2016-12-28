1d0314e6baa8017c31a539d0b46d8e34ba85d5e9
11Superman19 <pedro.95scpfialho@gmail.com>
2016-12-28 17:13:50 +0000

    Keep the try to obtain bitmap

--- MyWeatherApp/app/src/main/java/com/example/pedrofialho/myweatherapp/presentation/widget/PackShotView.kt
+++ MyWeatherApp/app/src/main/java/com/example/pedrofialho/myweatherapp/presentation/widget/PackShotView.kt
@@ -2,16 +2,20 @@ package com.example.pedrofialho.myweatherapp.presentation.widget
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.util.AttributeSet
 import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.android.volley.toolbox.ImageLoader
 import com.android.volley.toolbox.NetworkImageView
+import com.android.volley.toolbox.RequestFuture
 import com.example.pedrofialho.myweatherapp.R
 import com.example.pedrofialho.myweatherapp.WeatherApplication
 import com.example.pedrofialho.myweatherapp.model.WeatherDetails
 import com.example.pedrofialho.myweatherapp.model.WeatherForecast
+import java.net.URL
 
 
 class PackShotView(ctx: Context, attrs: AttributeSet?, defStyle: Int) : LinearLayout(ctx, attrs, defStyle) {
@@ -36,17 +40,35 @@ class PackShotView(ctx: Context, attrs: AttributeSet?, defStyle: Int) : LinearLa
     fun setWeatherInfo(weatherDetail: WeatherDetails?,
                      imageLoader: ImageLoader,
                      urlBuilder: String) {
-
+        val future: RequestFuture<Bitmap> = RequestFuture.newFuture()
         (findViewById(R.id.weatherTitle) as TextView).text = weatherDetail?.weather!![0].description
             val url = urlBuilder
+            var bitmap : Bitmap? = null
             Log.v(resources.getString(R.string.app_name), "Displaying image from URL $url")
+        (object : AsyncTask<String,Unit,Bitmap>(){
+            override fun doInBackground(vararg params: String?): Bitmap {
+                val mUrl = URL(urlBuilder)
+                val image = BitmapFactory.decodeStream(mUrl.openConnection().inputStream)
+                return image
+            }
 
-            (findViewById(R.id.packShotImage) as NetworkImageView).setImageUrl(url,imageLoader)
+            override fun onPostExecute(result: Bitmap?) {
+                bitmap = result
+            }
+        }).execute()
+        getBitmap(url,bitmap)
+            (findViewById(R.id.packShotImage) as NetworkImageView).setImageBitmap(bitmap)
+    }
+    fun getBitmap(key: String?,bitmap: Bitmap?) : Bitmap?{
+        val newBitmap : Bitmap? = getBitmapFromMemCache(key)
+        if(newBitmap == null){
+            addBitmapToMemoryCache(key,bitmap)
+            return bitmap
+        }
+        return newBitmap
     }
     fun addBitmapToMemoryCache(key : String?, bitmap: Bitmap?){
-        if(getBitmapFromMemCache(key) == null){
             (this.context.applicationContext as WeatherApplication).mMemoryCache.putBitmap(key,bitmap)
-        }
     }
 
     fun getBitmapFromMemCache(key: String?): Bitmap? {
